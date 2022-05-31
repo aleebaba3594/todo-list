e5175d03e365b02040b4d5b3dac91e1e699afef9
Ali Ahmed <aliofficial3594@gmail.com>
2022-05-31 21:57:08 +0500

    updated

--- index.js
+++ index.js
@@ -22,14 +22,44 @@ const to_print = () => {
   const getdata = JSON.parse(localStorage.getItem("user"));
 
   show.innerHTML = "";
-  getdata.forEach((e, i) => {
+  getdata.map((e, i) => {
     show.innerHTML += `<h2 class="lis">${e.myinput}
-        <button onclick="del(${i})" >del</button>
-        <button onclick="edit(${i})">edit</button>
-        <button onclick="update(${i})">update</button>
+        <button class="btn_output" onclick="del(${i})" >del</button>
+        <button class="btn_output" onclick="edit(${i})">edit</button>
+        <button class="btn_output" onclick="update(${i})">update</button>
+        <button class="btn_output" onclick="shiftUp(${i})">shift up</button>
+        <button class="btn_output" onclick="shiftDown(${i})">shift down</button>
+
         </h2>`;
   });
 };
+const shiftUp = (i) => {
+  console.log(i);
+  const getdata = JSON.parse(localStorage.getItem("user"));
+  console.log(getdata, "getdata");
+  if (i == 0) {
+    alert("you are already at top index");
+  } else {
+    var temp = getdata[i];
+    getdata[i] = getdata[i - 1];
+    getdata[i - 1] = temp;
+    localStorage.setItem("user", JSON.stringify(getdata));
+    location.reload();
+  }
+};
+function shiftDown(i) {
+  const getdata = JSON.parse(localStorage.getItem("user"));
+  if (i == getdata.length - 1) {
+    alert("you are already at bottom index");
+  } else {
+    var temp = getdata[i];
+    getdata[i] = getdata[i + 1];
+    getdata[i + 1] = temp;
+    localStorage.setItem("user", JSON.stringify(getdata));
+    location.reload();
+  }
+}
+
 const del = (i) => {
   const getdata = JSON.parse(localStorage.getItem("user"));
   getdata.splice(i, 1);
@@ -70,20 +100,20 @@ to_print();
 // ------------------joshuwa method and i reverse it to make it easyily understand-----------
 
 // var myinput = document.getElementById("myInput").value;
-  
-  // if(myinput.trim() != 0)
-  // {
-  //   let webtask  = localStorage.getItem('user')
-  //   if(webtask == null)
-  //   {
-  //     taskobj = []
 
-  //   }
-  //   else
-  //   {
-  //     taskobj = JSON.parse(webtask)
-  //   }
-  //   taskobj.push({"task":myinput})
-    
-  //   localStorage.setItem('user',JSON.stringify(taskobj))
-  // }
+// if(myinput.trim() != 0)
+// {
+//   let webtask  = localStorage.getItem('user')
+//   if(webtask == null)
+//   {
+//     taskobj = []
+
+//   }
+//   else
+//   {
+//     taskobj = JSON.parse(webtask)
+//   }
+//   taskobj.push({"task":myinput})
+
+//   localStorage.setItem('user',JSON.stringify(taskobj))
+// }
